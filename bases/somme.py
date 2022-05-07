1a45af11b7c2b2cd33d0b9b820402b26e76c9d31
mudjaycker <tombutoyi@gmail.com>
2022-05-07 19:48:13 +0200

    add some exercices

---
+++ bases/somme.py
@@ -0,0 +1,18 @@
+kalist = [3, 9, 14, 18, 2003, 66]
+
+
+# somme = 0
+
+# for i in kalist:
+# 	somme += i
+
+# print(somme)
+# a = sum(kalist)
+# print(a)
+
+fact = 1
+for i in range(1,5):
+	fact *= (i+1)
+
+
+# next day we'll learn conditions and functions
