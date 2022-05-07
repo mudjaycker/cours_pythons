65d4fa2d5b248c28c980c255a6cb8353f8ce7e58
mudjaycker <tombutoyi@gmail.com>
2022-05-07 10:42:51 +0200

    type de base

---
+++ bases.py
@@ -0,0 +1,18 @@
+entier = 7
+
+decimale = 8.9976
+
+izina = "J'ai"
+
+urutonde = [2, 6, 8]
+#           0  1  2
+
+kazinduzi = {
+	"nom": "tom",
+	"prenom": "jerry",
+	"age": 12,
+	"poids": 79.7
+}
+
+
+print(kazinduzi)
