4f547303c74778b784c41a3b1b42f6bdcec4bf90
mudjaycker <tombutoyi@gmail.com>
2022-05-08 09:43:55 +0200

    dico exemple 2

--- bases/dictio.py
+++ bases/dictio.py
@@ -8,5 +8,10 @@ dictionnaire = {
 # print(x)
 # print(dictionnaire["province"])
 
-for k in dictionnaire:
-	print(f"{k} ==> {dictionnaire[k]}")
+# for k in dictionnaire:
+# 	print(f"{k} ==> {dictionnaire[k]}")
+
+
+
+for k, v in dictionnaire.items():
+	print(f"{k} => {v}")
