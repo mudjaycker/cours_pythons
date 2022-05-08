208bc81022c39d505058cce1544c2504e98568f7
mudjaycker <tombutoyi@gmail.com>
2022-05-08 09:36:50 +0200

    dico exemple 1

---
+++ bases/dictio.py
@@ -0,0 +1,12 @@
+dictionnaire = {
+	"izina": "Butoyi",
+	"amatazirano": "Tom",
+	"province": "Gitega"
+}
+
+# x = dictionnaire["izina"]dictionnaire["province"]
+# print(x)
+# print(dictionnaire["province"])
+
+for k in dictionnaire:
+	print(f"{k} ==> {dictionnaire[k]}")
