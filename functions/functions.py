4d14231cb62b595baa91a12cdf83fd826b469a31
mudjaycker <tombutoyi@gmail.com>
2022-05-08 09:02:13 +0200

    begining with functions

---
+++ functions/functions.py
@@ -0,0 +1,21 @@
+# ama fonctions
+
+# fonction pour la somme
+def somme(tableau):
+	s = 0
+	for i in tableau:
+		s = s+i
+	print(s)
+
+
+tab = [1,4,6,3]
+# somme(tableau=tab)
+
+tab2 = [4.6 , 8.9, 10.11]
+
+
+def soustraction(a, b):
+	print(a-b)
+
+
+soustraction(b=6, a=8)
